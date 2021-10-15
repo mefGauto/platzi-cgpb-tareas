eaa37aee2f13d0a9dbe1b4ebe626b72d885c6fc2
Matías <90357238+mefGauto@users.noreply.github.com>
2021-10-15 16:55:38 -0300

    realizacion del reto que propuso el instructor

--- Canvas/dibujo.js
+++ Canvas/dibujo.js
@@ -1,7 +1,15 @@
-var d = document.querySelector('#canvas');
-var lienzo = d.getContext("2d");
+let d = document.querySelector('#canvas');
+let lienzo = d.getContext("2d");
+let lineas = 30;
+let xI;
+let yF;
 
-dibujarLinea("pink", 15, 78, 255, 25)
+for (let i = 0; i < lineas; i++){
+  dibujarLinea("orange", xI, 0, 300, yF)
+  xI = 10 * i;
+  yF = 10 * (i+ 1)
+}
+// dibujarLinea("pink", 15, 78, 255, 25)
 
 function dibujarLinea(color, xInicial, yInicial, xFinal, yFinal){
   lienzo.beginPath();
