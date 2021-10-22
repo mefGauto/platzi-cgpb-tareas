f2d8045e0b4944b11680aad41b265bc73ee385f5
Matías <90357238+mefGauto@users.noreply.github.com>
2021-10-22 20:43:51 -0300

    Añade la función que activan los objetos
    Actualiza el fondo para convertirlo en objeto tambien y añade la funcion que disparan los objetos, la funcion cambian el estado de carga de false a true

--- Villa platzi/villa-platzi.js
+++ Villa platzi/villa-platzi.js
@@ -1,17 +1,20 @@
 let canvas = document.querySelector('#canvas');
 let lienzo = canvas.getContext('2d');
-let mapa = 'img/tile.png';
 
-let fondo = new Image();
-fondo.src = mapa;
-fondo.addEventListener('load', dibujar);
+let fondo = {
+  url: 'img/tile.png',
+  carga: false
+};
+fondo.imagen = new Image();
+fondo.imagen.src = fondo.url;
+fondo.imagen.addEventListener('load', cargarFondo);
 
 let vaca = {
   url: 'img/vaca.png',
   carga: false
 };
 vaca.imagen = new Image();
-vaca.imagen.src = 'img/vaca.png';
+vaca.imagen.src = vaca.url;
 vaca.imagen.addEventListener('load', cargarVacas);
 
 let cerdo = {
@@ -19,7 +22,7 @@ let cerdo = {
   carga: false
 };
 cerdo.imagen = new Image();
-cerdo.imagen.src = 'img/cerdo.png';
+cerdo.imagen.src = cerdo.url;
 cerdo.imagen.addEventListener('load', cargarCerdos);
 
 let pollo = {
@@ -27,8 +30,21 @@ let pollo = {
   carga: false
 };
 pollo.imagen = new Image();
-pollo.src.imagen = 'img/pollo.png';
+pollo.src.imagen = pollo.url;
 pollo.imagen.addEventListener('load', cargarPollos);
+
+function cargarFondo(){
+  fondo.carga = true;
+}
+function cargarVacas(){
+  vaca.carga = true;
+}
+function cargarCerdos(){
+  cerdo.carga = true;
+}
+function cargarPollos(){
+  pollo.carga = true;
+}
 
 function dibujar(){
   lienzo.drawImage(fondo, 0, 0);
