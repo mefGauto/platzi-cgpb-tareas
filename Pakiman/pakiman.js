2b4f54223a33218555ea46f64a2db78d876dde27
Matías <90357238+mefGauto@users.noreply.github.com>
2021-10-25 20:00:15 -0300

    crea la clase y añade los stats

---
+++ Pakiman/pakiman.js
@@ -0,0 +1,11 @@
+class Pakiman {
+  stats(n, v, a){
+    this.nombre = n;
+    this.vida = v;
+    this.ataque = a;
+  }
+}
+
+let vaca = new Pakiman('Ternita', 180, 40);
+let pollo = new Pakiman('MoronMoron', 50, 200,)
+let cerdo = new Pakiman('Bostercito', 100, 60)
