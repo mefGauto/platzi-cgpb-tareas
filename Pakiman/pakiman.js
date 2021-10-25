a2a77118acfad14c7ad977f0348d8baf754fa450
Matías <90357238+mefGauto@users.noreply.github.com>
2021-10-25 20:42:19 -0300

    Intento de agregar la imagen al body
    Por alguna razon no logro que la imagen aparezca en el body con el appendChild

--- Pakiman/pakiman.js
+++ Pakiman/pakiman.js
@@ -1,11 +1,23 @@
+let imagenes = [];
+imagenes['Ternita'] = 'img/vaca.png';
+imagenes['MoronMoron'] = 'img/pollo.png';
+imagenes['Bostercito'] = 'img/cerdo.png';
+
 class Pakiman {
   stats(n, v, a){
+    this.imagen = new Image();
     this.nombre = n;
     this.vida = v;
     this.ataque = a;
+
+    this.imagen.src = imagenes[this.nombre];
+  }
+  mostrar(){
+    document.appendChild(this.imagen);
   }
 }
 
-let vaca = new Pakiman('Ternita', 180, 40);
-let pollo = new Pakiman('MoronMoron', 50, 200,)
-let cerdo = new Pakiman('Bostercito', 100, 60)
+let tiernita = new Pakiman('Ternita', 180, 40);
+let moronMoron = new Pakiman('MoronMoron', 50, 200,);
+let bostercito = new Pakiman('Bostercito', 100, 60);
+tiernita.mostrar();
