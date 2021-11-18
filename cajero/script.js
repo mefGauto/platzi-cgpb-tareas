0a78291feb186d9f0be9c123101a2059c39895db
mefGauto <gautomatiaz@gmail.com>
2021-11-18 10:34:51 -0300

    Añade los billetes extraidos al body Se cambia el codigo para permitirle al usuario ver que cantidad de billetes y de que valor extrajo.  En el ejercicio el parrafo donde se guarda el resultado estaba predefinido en html, yo trate de crearlo desde js pero me salta un bug que no toma la etiqueta <br />.

--- cajero/script.js
+++ cajero/script.js
@@ -18,15 +18,19 @@ let div = 0;
 
 let papeles = 0;
 
+let r = document.createElement('p');
+let body = document.querySelector('body');
+body.appendChild(r);
+
 let b = document.querySelector('#extraer');
 b.addEventListener('click', entregarDinero);
 
 function entregarDinero(){
-
+    let bi;
     let t = document.querySelector('#plata');
     dinero = parseInt(t.value);
 
-    for(let bi of caja){
+    for(bi of caja){
 
         if(dinero > 0){
             div = Math.floor(dinero / bi.valor);
@@ -43,6 +47,11 @@ function entregarDinero(){
     }
     if (dinero > 0){
         console.log('Si me pones mas plata mejor pa');
+    }else{
+        for(let e of entregado)
+        {
+            let billeteEntregado = ( e.cantidad + ' billetes de $' + e.valor + '<br />' );
+            r.append(billeteEntregado);
+        }
     }
-    console.log(entregado);
 }
