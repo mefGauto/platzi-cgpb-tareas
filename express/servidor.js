ef1c05e06e3b223548aa11dbaf2afcfa72e47dbc
mefGauto <gautomatiaz@gmail.com>
2021-11-20 16:13:41 -0300

    Añade los archivos necesarios para correr express

--- express/servidor.js
+++ express/servidor.js
@@ -1,2 +1,11 @@
 let express = require('express');
 let app = express();
+
+app.get('/', inicio);
+
+function inicio(peticion, resultado)
+{
+    resultado.send('Este es el <strong>home</strong>');
+}
+
+app.listen(8989);
